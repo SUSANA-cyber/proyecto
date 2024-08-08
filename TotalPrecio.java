a775599767e8fc41f7cf18e59a2fcbf1eb3a57ef
SUSANA-cyber <slopezv21@miumg.edu.gt>
2024-08-07 20:27:42 -0600

    mi primer repositorio

---
+++ TotalPrecio.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TotalPrecio{
+
+public static void main(String [] args){
+
+Scanner scanner = new Scanner(System.in);
+
+double precio,totalDescuento,precioFinal;
+final double iva = 0.12;
+final double descuento = 0.20;
+
+System.out.print("muestre la cantidad a cobrar: ");
+precio = scanner.nextDouble();
+
+totalDescuento = precio * (1-0.20);
+//mostrar el monto final del descuento
+precioFinal = totalDescuento * (0.12);
+
+System.out.println("Precio descuento:" + totalDescuento);
+
+System.out.println("Precio final con IVA:" + precioFinal);
+}
+}
